66eab8df39c010d238e0cf593a45d3fa65ad6f26
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-24 22:42:38 -0600

    Route for WeatherController

---
+++ app/controllers/api/v1/weather_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::WeatherController < ApplicationController
+  def forecast
+
+  end 
+end
---
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Weather API' do
+  it 'gets current, hourly and daily weather for a location' do
+    locale = 'lincoln,ne'
+    get "/api/v1/forecast?location=#{locale}"
+
+    expect(response).to be_successful
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    weather = response_body[:data]
+  end
+end
