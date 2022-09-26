39a519f0143e5745d9cad7639b2ae6c448a65e61
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-26 11:05:49 -0600

    Still not passing

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -6,12 +6,36 @@ RSpec.describe BookService do
 
     expect(response).to be_a Hash
     expect(response.keys).to eq([:numFound, :start, :numFoundExact, :docs, :num_found, :q, :offset])
-    expect(response[:numFound].count).to eq 38
+    expect(response[:numFound]).to eq 38
     expect(response[:docs].count).to eq 38
       response[:docs].each do |doc|
         expect(doc).to have_key(:title)
-        expect(doc).to have_key(:seed)
-require "pry"; binding.pry
+        expect(doc[:title].downcase).to include('denver')
+        expect(doc.keys).to eq([:key,
+                                :type,
+                                :seed,
+                                :title,
+                                :title_suggest,
+                                :subtitle,
+                                :edition_count,
+                                :edition_key,
+                                :publish_date,
+                                :publish_year,
+                                :first_publish_year,
+                                :isbn,
+                                :last_modified_i,
+                                :ebook_count_i,
+                                :ebook_access,
+                                :has_fulltext,
+                                :public_scan_b,
+                                :publisher,
+                                :language,
+                                :author_key,
+                                :author_name,
+                                :author_alternative_name,
+                                :publisher_facet,
+                                :_version_,
+                                :author_facet])
       end
   end
 end
