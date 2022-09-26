a0d1f092d5bafc9d7615befd13b20fccdc00dcd7
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-26 17:34:05 -0600

    forecast_details_hash in openstruct half values

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,23 +1,32 @@
 class BookFacade
   def self.destination_forecast(location)
     locale = MapService.get_location(location)
-    forecast = WeatherService.get_weather(locale[:lat], locale[:lng])
-    books = BookService.get_books(location)
+    @forecast_data = WeatherService.get_weather(locale[:lat], locale[:lng])
+    book_data = BookService.get_books(location)
 
     data = OpenStruct.new({
       id: nil,
       type: 'books',
       destination: get_destination(location),
-      :forecast => {
-        summary: forecast[:current][:weather][0][:description],
-        temperature: forecast[:current][:temp].to_s + 'F'
-      }
+      forecast: forecast_details_hash,
+      total_books_found: book_data[:numFound],
+      books: book_data[:docs].take(5)
       })
-      require "pry"; binding.pry
   end
 
   def self.get_destination(location)
     location
   end
 
+  def self.forecast_details_hash
+    forecast_details = {}
+    forecast_details.store(:summary, @forecast_data[:current][:weather][0][:description])
+    forecast_details.store(:temperature, @forecast_data[:current][:temp].to_s + ' F')
+  end
+
+  def self.book_details
+    book_data[:docs].take(5).map do |doc|
+      require "pry"; binding.pry
+    end
+  end
 end
--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe BookFacade do
   it 'is books that contain Denver, CO in title and weather', :vcr do
     response = BookFacade.destination_forecast('denver,co')
 
+require "pry"; binding.pry
     expect(response.id).to be_nil
+    expect(response).to be_an OpenStruct
   end
 end
