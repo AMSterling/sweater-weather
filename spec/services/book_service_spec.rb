fc39113f60d037f27d0ed1c1bf6360df1c70a356
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-26 10:44:43 -0600

    :(

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe BookService do
   it 'gets books by location', :vcr do
     response = BookService.get_books('denver,co')
-require "pry"; binding.pry
+
     expect(response).to be_a Hash
     expect(response.keys).to eq([:numFound, :start, :numFoundExact, :docs, :num_found, :q, :offset])
+    expect(response[:docs].count).to eq 38
+      response[:docs].each do |doc|
+        expect(doc).to have_key(:title)
+require "pry"; binding.pry
+      end
   end
 end
