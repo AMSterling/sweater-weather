dc6dc66391b5cab7f906ce2eb811632ea48bdaa1
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-29 14:58:23 -0600

    Reformat user create controller to handle sad path

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    new_user = user_params
-    new_user[:email] = user_params[:email]
-    user = User.create!(new_user)
-      json = render json: UsersSerializer.new(user), status: :created
+    if params[:password] != params[:password_confirmation]
+      render status: 422
+    elsif
+      new_user = user_params
+      new_user[:email] = user_params[:email]
+        user = User.new(new_user)
+      if user.save
+        render json: UsersSerializer.new(user), status: :created
+      else
+        render json: user.errors.full_messages, status: 422
+      end
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :users, only: %i[create]
+      resource :users, only: [:create]
       get :forecast, to: 'weather#forecast'
     end
   end
