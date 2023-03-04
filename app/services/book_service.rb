67b8a2666b65e45f5f813bfed38fe10071609065
Anna Marie Sterling <sterling.am@gmail.com>
2023-03-04 00:11:35 -0700

    Add book service

---
+++ app/services/book_service.rb
@@ -0,0 +1,19 @@
+class BookService
+  def self.get_books(location, quantity)
+    response = conn.get('search.json?') do |faraday|
+      faraday.params['title'] = location
+    end
+    data = parse(response)
+  end
+
+
+  private
+
+  def self.conn
+    connection = Faraday.new(url: 'http://openlibrary.org/')
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
---
+++ spec/services/book_service_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe BookService do
+  it 'gets books by location', :vcr do
+    response = BookService.get_books('denver,co', 5)
+
+    expect(response).to be_a Hash
+    expect(response.keys).to eq([:numFound, :start, :numFoundExact, :docs, :num_found, :q, :offset])
+    expect(response[:numFound]).to be_an Integer
+    expect(response[:docs].count).to eq 44
+      response[:docs].each do |doc|
+        expect(doc).to have_key(:title)
+        expect(doc[:title].downcase).to include('denver')
+        expect(doc.has_key?(:isbn))
+        expect(doc.has_key?(:publisher))
+      end
+  end
+end
