402edb5045dd50fac4e2002e8c46af5ebbff068e
Anna Marie Sterling <sterling.am@gmail.com>
2023-03-04 00:15:11 -0700

    Add book facade

---
+++ app/facades/book_facade.rb
@@ -0,0 +1,34 @@
+class BookFacade
+  def self.destination_forecast(location, quantity)
+    locale = MapService.get_location(location)
+    @forecast_data = WeatherService.get_weather(locale[:lat], locale[:lng])
+    @book_data = BookService.get_books(location, quantity)
+
+    data = OpenStruct.new({
+      id: nil,
+      type: 'books',
+      attributes: {
+        destination: location,
+        forecast: forecast_details,
+        total_books_found: @book_data[:numFound],
+        books: book_details(location, quantity)
+      }})
+  end
+
+  def self.forecast_details
+    forecast_details_hash = {
+      summary: @forecast_data[:current][:weather][0][:description],
+      temperature: @forecast_data[:current][:temp].to_s + ' F'
+    }
+  end
+
+  def self.book_details(location, quantity)
+    @book_data[:docs].take(quantity.to_i).map do |doc|
+      book_details_hash = {
+        isbn: doc[:isbn],
+        title: doc[:title],
+        publisher: doc[:publisher]
+      }
+    end
+  end
+end
---
+++ spec/facades/book_facade_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe BookFacade do
+  it 'is books that contain Denver, CO in title and weather', :vcr do
+    response = BookFacade.destination_forecast('denver,co', 5)
+
+    expect(response).to be_an OpenStruct
+    expect(response.id).to be_nil
+    expect(response.type).to eq('books')
+    expect(response.attributes).to be_a Hash
+    expect(response.attributes).to have_key(:destination)
+    expect(response.attributes[:destination]).to eq('denver,co')
+    expect(response.attributes).to have_key(:forecast)
+    expect(response.attributes[:forecast]).to be_a Hash
+    expect(response.attributes[:forecast]).to have_key(:summary)
+    expect(response.attributes[:forecast][:summary]).to be_a String
+    expect(response.attributes[:forecast]).to have_key(:temperature)
+    expect(response.attributes[:forecast][:temperature]).to be_a String
+    expect(response.attributes).to have_key(:total_books_found)
+    expect(response.attributes).to have_key(:books)
+    expect(response.attributes[:books].count).to eq 5
+    response.attributes[:books].each do |book|
+      expect(book).to have_key(:isbn)
+      expect(book[:isbn]).to be_an Array
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a String
+      expect(book[:title].downcase).to include('denver')
+      expect(book).to have_key(:publisher)
+      expect(book[:publisher]).to be_an Array
+    end
+  end
+end
